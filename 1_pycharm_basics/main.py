5522a6f1aa424711458bd6d2f665b76a09511d13
KemalGunay <kemalgnay@gmail.com>
2024-09-12 21:27:47 +0300

    firt lesson added

---
+++ 1_pycharm_basics/main.py
@@ -0,0 +1,29 @@
+from person import Person
+
+name = "elia"
+name2 = "mia"
+name3 = "lola"
+
+def hola(name: str):
+    print(name.upper())
+
+def hola(name: str):
+    print(name.upper())
+
+from math import add
+
+if __name__ == '__main__':
+    # TODO: fix bug
+    # Todo: implement tests
+    print("mestan is awesome")
+    print("kemal is awesome")
+    print("PYCHARM is awesome")
+    print(add(2, 2))
+
+    alex = Person("alex")
+    print(alex)
+
+def kemal_method(name):
+    p = Person(name)
+    print(p)
+
---
+++ 1_pycharm_basics/test_gender_converter.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from gender_converter import convert_gender
+
+
+class Test(TestCase):
+    def test_convert_gender_when_male(self):
+        expected = "MALE"
+        actual = convert_gender("M")
+        self.assertEqual(actual, expected)
+
+    def test_convert_gender_when_female(self):
+        expected = "FEMALE"
+        actual = convert_gender("F")
+        self.assertEqual(actual, expected)
+
+
+    def test_convert_gender_when_unknown(self):
+        expected = "UNKNOWN_GENDER"
+        actual = convert_gender("HELLO")
+        self.assertEqual(actual, expected)
