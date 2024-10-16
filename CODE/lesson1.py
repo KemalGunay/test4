cf297a32a181d1ef8887a32934ccda3cc5c83c8f
KemalGunay <kemalgnay@gmail.com>
2024-10-16 11:52:15 +0300

    README.md dosyasını güncelledim

---
+++ CODE/lesson1.py
@@ -0,0 +1,51 @@
+# here is script section
+
+# this is comment
+# hashtag gives us to write our comment
+print("hello ai")
+
+# cmd + n open a new file
+# pycharm -> setting or preferences -> apperance -> theme
+
+#print("hello ai")
+
+
+
+# cmd + c  copy
+# cmd + v paste
+# cmd + x cut
+# cnd + z brint back your code
+
+print("kemal")
+
+
+variable1 = "kemal"
+variable1
+
+
+
+var2 = "lkil ldkglfkgj klagislfkg silk ilsfk"
+var2
+
+
+x = "kemal"
+y = "mehmet"
+z = "sevgi"
+
+
+print(x)
+print(y)
+print(z)
+
+print(x, y, z)
+
+
+print("goodbye class")
+
+
+a = 3 # integer
+b = 3.2 # float
+c = 3.46 # float
+d = "kemal" # string
+
+print(a, b, c, d)
